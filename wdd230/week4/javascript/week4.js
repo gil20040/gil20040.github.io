c0ef687837d03e8eafa615cb9c39beef1d50bc8b
gil20040 <gil20040@byui.edu>
2021-05-14 22:01:30 -0600

    commit

--- wdd230/week4/javascript/week4.js
+++ wdd230/week4/javascript/week4.js
@@ -1,3 +1,12 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+function siteDate() {
+const datefield = document.querySelector("date");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-us", { dateStyle: "full"}).format (now);
+datefield.textContent = fulldate;
+}
+window.onload = siteDate;
