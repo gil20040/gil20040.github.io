e44a6d34c924923a09680b717d69f2c5844abdb4
gil20040 <gil20040@byui.edu>
2021-05-22 00:06:00 -0600

    commit

--- wdd230/week5/js/banner.js
+++ wdd230/week5/js/banner.js
@@ -1,6 +1,6 @@
 let thedate = new Date();
 //console.log(thedate.getDay());
 
-if (thedate.getDay() == 6) {
+if (thedate.getDay() == 5) {
 document.querySelector('#banner').style.display = 'block';
 }
