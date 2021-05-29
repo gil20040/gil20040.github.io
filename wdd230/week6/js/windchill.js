e8a055969f7439d4643bae4ba5facbc35c6bf8d4
gil20040 <gil20040@byui.edu>
2021-05-29 15:25:51 -0600

    commit

--- wdd230/week6/js/windchill.js
+++ wdd230/week6/js/windchill.js
@@ -1,15 +1,15 @@
 function inputoutput() {
-    let intemp = 30 
-    let inwindspeed = 7 
+    let intemp = parseFloat(document.getElementById('temp').textContent);
+    let inwindspeed = parseFloat(document.getElementById('windspeed').textContent);
     
     let windChillFinal = windChill(intemp, inwindspeed);
     let windChillRound = (Math.round(parseFloat(windChillFinal)));
-    document.getElementById('windchill').innerHTML = windChillRound
+        document.getElementById('windchill').innerHTML = windChillRound
 }
 
 function windChill(intemp, inwindspeed) {
         let f = 35.74 + 0.6215 * intemp - 35.75 * Math.pow(inwindspeed,0.16) + 0.4275 * intemp * Math.pow(inwindspeed,0.16);
         return f;
     }
-window.onload = inputoutput;
     
+window.onload = inputoutput;
