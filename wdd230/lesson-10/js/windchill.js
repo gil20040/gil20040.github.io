94c81d2ac5a7a5453e96a4118fe3c6aa0e7c71d6
gil20040 <gil20040@byui.edu>
2021-06-25 23:30:45 -0600

    commit

--- wdd230/lesson-10/js/windchill.js
+++ wdd230/lesson-10/js/windchill.js
@@ -1,12 +1,12 @@
 function windChill(intemp, inwindspeed) {
          if (intemp > 50 || inwindspeed < 3) {
-        return 'None';
+        return 'N/A';
 }
-        return ( (Math.round(parseFloat(
+        return ((Math.round(parseFloat(
                 35.74 + 0.6215 * 
                 intemp - 35.75 * 
                 Math.pow(inwindspeed,0.16) + 
                 0.4275 * intemp * 
-                Math.pow(inwindspeed,0.16)))))
+                Math.pow(inwindspeed,0.16))))) + '° F'
     }
 
