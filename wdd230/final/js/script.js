f47bb9402a1f0e1fe5d88790e22cc67919d55229
gil20040 <gil20040@byui.edu>
2021-07-17 12:53:14 -0600

    commit

--- wdd230/final/js/script.js
+++ wdd230/final/js/script.js
@@ -2,29 +2,8 @@ function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 function gridView() {
-
+    document.getElementsByClassName("food")[0].style.display = "grid"
 }
 function listView() {
-    var f = document.getElementsByClassName("food")
-    var l = document.getElementsByClassName("foodlist")
-
-    if (l.style.display === "none") {
-        l.style.display = "block";
-        f.style.display = "none";
-    }
+    document.getElementsByClassName("food")[0].style.display = "block"
 }
-
-
-
-
-/*
-
-function gridView() {
-    document.getElementsByClassName('foodlist-section').style.display = 'none';
-    document.getElementsByClassName('food section').style.display = 'block';
-}
-function listView() {
-    document.getElementsByClassName('foodlist section').style.display = 'block';
-    document.getElementsByClassName('food section').style.display = 'none';
-}
-*/
