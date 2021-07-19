625b82d6b35e77d9338af4753dae20f31a7f43df
gil20040 <gil20040@byui.edu>
2021-07-18 21:56:16 -0600

    commit

--- wdd230/final/js/direct.js
+++ wdd230/final/js/direct.js
@@ -19,21 +19,27 @@ fetch(requestURL)
   let p = document.createElement('p');
   let p2 = document.createElement('p');
   let p3 = document.createElement('p');
+  let a = document.createElement('a');
   
   h2.textContent = foodbiz[i].bizname;
   img.setAttribute('src', foodbiz[i].imageurl); 
   img.setAttribute('alt',foodbiz[i].bizname);
-  img.setAttribute('id','foodimg')
+  img.setAttribute('class','foodi')
   p.textContent = foodbiz[i].phone;
   p2.textContent = foodbiz[i].address;
   p3.textContent = foodbiz[i].website;
+  a.setAttribute('href',foodbiz[i].website);
+  a.setAttribute('target','_blank');
+  a.setAttribute('class','sitelinks');
+  a.setAttribute('rel','noopener');
  
 
   card.appendChild(h2);
   card.appendChild(img);
   card.appendChild(p);
   card.appendChild(p2);
-  card.appendChild(p3);
+  card.appendChild(a);
+  a.appendChild(document.createTextNode("Web Site"));
 
 
   document.querySelector('div.food').append(card);
--- wdd230/final/js/script.js
+++ wdd230/final/js/script.js
@@ -2,8 +2,26 @@ function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 function gridView() {
-    document.getElementsByClassName("food")[0].style.display = "grid"
+    document.getElementsByClassName("food")[0].style.display = "grid";
+    document.getElementsByClassName("foodi")[0].style.display = "inline";
+    document.getElementsByClassName("foodi")[1].style.display = "inline";
+    document.getElementsByClassName("foodi")[2].style.display = "inline";
+    document.getElementsByClassName("foodi")[3].style.display = "inline";
+    document.getElementsByClassName("foodi")[4].style.display = "inline";
+    document.getElementsByClassName("foodi")[5].style.display = "inline";
+    document.getElementsByClassName("foodi")[6].style.display = "inline";
+    document.getElementsByClassName("foodi")[7].style.display = "inline";
+    document.getElementsByClassName("foodi")[8].style.display = "inline";
 }
 function listView() {
-    document.getElementsByClassName("food")[0].style.display = "block"
+    document.getElementsByClassName("food")[0].style.display = "block";
+    document.getElementsByClassName("foodi")[0].style.display = "none";
+    document.getElementsByClassName("foodi")[1].style.display = "none";
+    document.getElementsByClassName("foodi")[2].style.display = "none";
+    document.getElementsByClassName("foodi")[3].style.display = "none";
+    document.getElementsByClassName("foodi")[4].style.display = "none";
+    document.getElementsByClassName("foodi")[5].style.display = "none";
+    document.getElementsByClassName("foodi")[6].style.display = "none";
+    document.getElementsByClassName("foodi")[7].style.display = "none";
+    document.getElementsByClassName("foodi")[8].style.display = "none";
 }
